620f4679938d20f4a3de04804a6cd9e9189fd152
Chester <chestermaligaso29@gmail.com>
2025-03-10 12:52:43 +0800

    initialized project

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="w-full h-screen">
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+      </div>
+    </div>
+  )
+}
